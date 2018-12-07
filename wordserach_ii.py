895b2334a3c59d273b84c3568ad590f3b50f1f79
Anish0637 <45683634+Anish0637@users.noreply.github.com>
2018-12-07 22:25:23 +0530

    Update wordserach_ii.py

--- wordserach_ii.py
+++ wordserach_ii.py
@@ -1,34 +1,39 @@
-
-class Solution1:
-
-    def findWordss(self, board, words):
-        trie = {}#create an empty trie structure
+from functools import reduce
+from collections import defaultdict
+class Solution:
+    def findWords(self, board, words):
+        """
+        :type board: List[List[str]]
+        :type words: List[str]
+        :rtype: List[str]
+        """
+#        words="hack"
+        Trie = lambda : defaultdict(Trie)
+        trie = Trie()
         for w in words:
-            t = trie
-            for c in w:
-                if c not in t:
-                    t[c] = {}
-                t = t[c]
-            t['#'] = '#'
-        res = []
+            end = reduce(dict.__getitem__, w, trie)
+            end['#'] = '#'
+            end['$'] = w
+
+        self.res = set()
+        self.used = [[False] * len(board[0]) for _ in range(len(board))]
         for i in range(len(board)):
             for j in range(len(board[0])):
-                self.find(board, i, j, trie, '', res)# board,x,y direction,blank trie
-        return list(set(res))
+                self.find(board, i, j, trie)
+        return list(self.res)
 
-    def find(self, board, i, j, trie, path, res):
+    def find(self, board, i, j, trie):
         if '#' in trie:
-            res.append(path)
-        if i<0 or i>=len(board) or j<0 or j>=len(board[0]) or board[i][j] not in trie:
+            self.res.add(trie['$'])
+        if i < 0 or i >= len(board) or j < 0 or j >= len(board[0]):
             return
-        tmp = board[i][j]
-        board[i][j] ="@"
-        self.find(board, i+1, j, trie[tmp], path+tmp, res)
-        self.find(board, i, j+1, trie[tmp], path+tmp, res)
-        self.find(board, i-1, j, trie[tmp], path+tmp, res)
-        self.find(board, i, j-1, trie[tmp], path+tmp, res)
-        board[i][j] = tmp
-    
+        if not self.used[i][j] and board[i][j] in trie:
+            self.used[i][j] = True
+            self.find(board, i + 1, j, trie[board[i][j]])
+            self.find(board, i, j + 1, trie[board[i][j]])
+            self.find(board, i - 1, j, trie[board[i][j]])
+            self.find(board, i, j - 1, trie[board[i][j]])
+            self.used[i][j] = False
 
 words = ["oath","pea","eat","rain","oak","ath","kae","ner"] 
 board =[  ['o','a','a','n'],
@@ -40,7 +45,5 @@ board[0][0]
 board[1]
 type(words)
 type(board)
-l1=Solution1()
-l1.findWordss
-l1.findWordss(board,words)
-
+l=Solution()
+l.findWords(board,words)
